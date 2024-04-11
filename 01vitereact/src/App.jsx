4cc196c106c1d4f3352558384b00bfbd4fc083e3
stylebhai <sam9993545117@gmail.com>
2024-04-11 19:19:10 +0530

    react-series

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,18 @@
+import Chai from "./chai"
+
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Chai />
+    <h1>chai peete rahiye</h1>
+    <p>kaisi lagi chai</p>
+    </>
+    
+  )
+}
+
+export default App
