28ca1bbdc28f7ada7c054a14ded42da0f71a4cfd
stylebhai <sam9993545117@gmail.com>
2024-04-14 21:46:30 +0530

    tailwindandpropslearn

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -3,12 +3,12 @@ import Chai from "./chai"
 
 
 function App() {
-  
+  const username = "drink Tea"
 
   return (
     <>
     <Chai />
-    <h1>chai peete rahiye</h1>
+    <h1>chai peete rahiye {username}</h1>
     <p>kaisi lagi chai</p>
     </>
     
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+const anElement = (
+  <a href='https://google.com' target='_blank'>Visit Google</a>
+)
 
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  "Click me to visit Google"
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  
     <App />
-  </React.StrictMode>,
+  
 )
