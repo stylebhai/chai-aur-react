5aa0fbd7da21ac11c64601de8c3f175196ee717d
stylebhai <sam9993545117@gmail.com>
2024-04-15 12:09:36 +0530

    interview

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -9,7 +9,10 @@ function App() {
   const addValue = ()=> {
     //console.log("clicked", counter);
     //counter = counter + 1
-    setCounter(counter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
   }
   const removeValue = ()=> {
     //console.log("clicked", counter);
